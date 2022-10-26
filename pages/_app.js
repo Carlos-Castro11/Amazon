087dabaca76541b0f8bfc27580845eeb39c525f8
Carlos Henrique <carloshenriquemg2020@gmail.com>
2022-10-26 17:57:32 -0300

    correct

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
